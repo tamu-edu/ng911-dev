554b1b3ddf75e776dc9f2d0a2770026da2014e0b
loganfreeman-tamu <138712838+loganfreeman-tamu@users.noreply.github.com>
2025-08-08 14:34:38 -0500

    Delete test_suite directory

--- test_suite/tests/ESRP_003/constants.py
+++
@@ -1,5 +0,0 @@
-VARIATION_TO_NAME_MAPPING = {
-    'SIP_INVITE_from_OSP.xml': "no_geolocation_data",
-    'SIP_INVITE_location_garbled_PIDF-LO_body.xml': "garbled_pidf_lo",
-    'SIP_INVITE_incorrect_geolocation_for_dereference.xml': "geolocation_header_unable_to_dereference"
-}
