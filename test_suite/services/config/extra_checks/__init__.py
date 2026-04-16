ed606223a8d52305ac99a5ca3094dafffe69ae04
loganfreeman-tamu <loganf@tamu.edu>
2026-04-16 15:52:21 -0500

    clear for v5.1 release

--- test_suite/services/config/extra_checks/__init__.py
+++
@@ -1,5 +0,0 @@
-import pkgutil
-import importlib
-
-for module in pkgutil.iter_modules(__path__):
-    importlib.import_module(f"{__name__}.{module.name}")
--- test_suite/tests/BCF_005/constants.py
+++
@@ -1,8 +0,0 @@
-TEST_NAMES = [
-    "Validate BCF sends HTTP POST to /LogEvents",
-    "Validate BCF forwards SIP from OSP to ESRP",
-    "Validate Log Event depends on SIP MESSAGE",
-    "Validate Log Event depends on SIP Other message type",
-    "Validate Log Event direction values",
-]
-LOG_EVENTS_URI = "/LogEvents"
